7051dafee26e08bdb587ab158b60ecad8ea51e45
Henry Moss <htmoss7@gmail.com>
2020-05-19 16:43:09 -0700

    Second Commit
    - Updated color theme & style
    - List automatically reorders
    - Responsive on phones & desktop

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # BookRatingWebsite
+
 Created functional book rating website using HTML, CSS, &amp; JS
+
+Link:
+https://friendly-chandrasekhar-92a6dd.netlify.app/
--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+let ratingNum = document.querySelector('#rating-num');
+let rangeBar = document.querySelector('#input-rating');
+const submitBtn = document.getElementById('new-book-submit');
+const mainTable = document.getElementById('main-table');
+const bookTable = document.createElement('ul');
+const addInfo = document.querySelector('#add-info');
+
+// Showing the value of the range bar below the range bar
+rangeBar.addEventListener('change', function () {
+	ratingNum.textContent = `${rangeBar.value} out of 7`;
+});
+
+// original bookList to start off with
 const bookList = [
 	{
 		title: 'Early Riser',
@@ -10,16 +23,22 @@ const bookList = [
 		rating: 6,
 	},
 ];
-const bookTable = document.createElement('ul');
 
-const createBookTable = (bookList) => {
+// creates a book table from the book list
+const createBookTable = (bookList, tableLocation) => {
+	bookList.sort(function (a, b) {
+		return b.rating - a.rating;
+	});
+	clearBox(bookTable);
+	createLabelBoxes(bookTable);
 	for (book of bookList) {
-		addBook(book, bookTable);
+		createBook(book, bookTable);
 	}
-	document.getElementById('main-table').append(bookTable);
+	tableLocation.append(bookTable);
 };
 
-const addBook = ({ title, author, rating }, bookTable) => {
+// creates book item for each book in bookTable
+const createBook = ({ title, author, rating }, bookTable) => {
 	const bookLi = document.createElement('li');
 	bookTable.append(bookLi);
 
@@ -39,35 +58,76 @@ const addBook = ({ title, author, rating }, bookTable) => {
 	bookLi.append(ratingDiv);
 };
 
-createBookTable(bookList);
-
-const submitBtn = document.getElementById('new-book-submit');
+// Function for submit button, shows a warning if the entire rating has not been filled out
 submitBtn.addEventListener('click', function () {
-	console.log('I have been clicked');
 	const newBook = getBook();
 	console.log('newBook:', newBook);
 	if (newBook) {
+		// takes away warning if it is already there
 		if (document.querySelector('#warning').classList.contains('show')) {
 			document.querySelector('#warning').classList.remove('show');
 		}
 		console.log('adding book');
-		addBook(newBook, bookTable);
-		document.getElementById('main-table').append(bookTable);
+		createBookTable(bookList, mainTable);
 	}
 });
 
+// gets input from user rating and pushes it to bookList
 const getBook = () => {
 	let title = document.getElementById('input-title').value;
 	let author = document.getElementById('input-author').value;
-	let rating = document.getElementById('input-rating').value;
+	let rating = Number(document.getElementById('input-rating').value);
 	if (title && author && rating) {
 		const newBook = {
 			title,
 			author,
 			rating,
 		};
+		bookList.push(newBook);
 		return newBook;
 	} else {
 		document.querySelector('#warning').classList.add('show');
 	}
 };
+
+function clearBox(element) {
+	console.log('Clearing!');
+	element.innerHTML = '';
+}
+
+function createLabelBoxes(bookTable) {
+	const bookLi = document.createElement('li');
+	bookTable.append(bookLi);
+
+	const titleDiv = document.createElement('div');
+	const titleIcon = document.createElement('i');
+	const titleP = document.createElement('p');
+	titleDiv.classList.add('topBox');
+	titleIcon.classList.add('fas', 'fa-book');
+	titleP.innerText = 'Title';
+	bookLi.append(titleDiv);
+	titleDiv.append(titleIcon);
+	titleDiv.append(titleP);
+
+	const authorDiv = document.createElement('div');
+	const authorIcon = document.createElement('i');
+	const authorP = document.createElement('p');
+	authorDiv.classList.add('topBox');
+	authorIcon.classList.add('fas', 'fa-user-alt');
+	authorP.innerText = 'Author';
+	bookLi.append(authorDiv);
+	authorDiv.append(authorIcon);
+	authorDiv.append(authorP);
+
+	const ratingDiv = document.createElement('div');
+	const ratingIcon = document.createElement('i');
+	const ratingP = document.createElement('p');
+	ratingDiv.classList.add('topBox');
+	ratingIcon.classList.add('fas', 'fa-star-half-alt');
+	ratingP.innerText = 'Rating';
+	bookLi.append(ratingDiv);
+	ratingDiv.append(ratingIcon);
+	ratingDiv.append(ratingP);
+}
+
+createBookTable(bookList, mainTable);
